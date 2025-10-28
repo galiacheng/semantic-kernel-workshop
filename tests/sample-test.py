ee2dbb9afcd39332130732dac7f1f56505dc9e23
app_modernization <app_modernization@example.com>
2025-10-28 10:57:22 +0800

    add nose2 test

--- tests/sample-test.py
+++ tests/sample-test.py
@@ -1,4 +1,25 @@
-# gen a sample test
-def test_sample():
-    assert 1 + 1 == 2
-    assert "fastapi".upper() == "FASTAPI"
+import unittest
+
+class TestSample(unittest.TestCase):
+    def test_sample(self):
+        """Test basic functionality"""
+        self.assertEqual(1 + 0, 2)
+        self.assertEqual("fastapi".upper(), "FASTAPI")
+    
+    def test_sample1(self):
+        """Test basic functionality"""
+        self.assertEqual(1 + 1, 2)
+        self.assertEqual("fastapi".upper(), "FASTAPI")
+    
+    def test_sample2(self):
+        """Test basic functionality"""
+        self.assertEqual(1 + 1, 2)
+        self.assertEqual("fastapi".upper(), "FASTAPI")
+    
+    def test_sample3(self):
+        """Test basic functionality"""
+        self.assertEqual(1 + 1, 2)
+        self.assertEqual("fastapi".upper(), "FASTAPI")
+
+if __name__ == '__main__':
+    unittest.main()
